20572a4c23d52d649f82e82bb2259d6a31acdd54
Russkazkin <ruslan@skazkin.su>
2020-10-31 16:41:56 +0300

    Add some question styling

--- lib/question.dart
+++ lib/question.dart
@@ -8,10 +8,14 @@ class Question extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      width: double.infinity,
       padding: EdgeInsets.all(20.0),
+      margin: EdgeInsets.only(bottom: 20.0),
+      decoration: new BoxDecoration(color: Colors.orange),
       child: Text(
         _questionText,
         style: TextStyle(fontSize: 20.0),
+        textAlign: TextAlign.center,
       ),
     );
   }
